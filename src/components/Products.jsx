c81b7ac9831ca1acf7e72227651bfa3d9486f6b5
Julian Barbosa <julianbarbossa@hotmail.com>
2023-11-15 18:35:24 -0500

    Add Products component with table structure

---
+++ src/components/Products.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Products = () => {
+  return (
+    <>
+      <h2 className='text-center my-5'>List Products</h2>
+
+      <table className='table table-striped'>
+        <thead className='bg-primary table-dark'>
+          <tr>
+            <th scope='col'>Name</th>
+            <th scope='col'>Price</th>
+            <th scope='col'>Actions</th>
+          </tr>
+        </thead>
+        
+        <tbody>
+
+        </tbody>
+      </table>
+    </>
+  )
+}
+
+export default Products
