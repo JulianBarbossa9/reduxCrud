7d8d28a6e0be92f09351511cf47c207b3f477541
Julian Barbosa <julianbarbossa@hotmail.com>
2023-11-17 12:05:11 -0500

    Add useEffect and useSelector to Products component

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { getAllProducts } from '../actions/productsActions'
+import Product from './Product'
 
 const Products = () => {
+
+  const dispatch = useDispatch()
+  
+  
+  
+  useEffect(() => {
+    const downloadProducts = () => dispatch(getAllProducts())
+    downloadProducts()
+  },[])
+  
+  const allProducts = useSelector((state) => state.products.products)
+  // console.log(donwloadProducts)
+  const error = useSelector((state) => state.products.error)
+  const loading = useSelector((state) => state.products.loading)
+  
   return (
     <>
       <h2 className='text-center my-5'>List Products</h2>
+
+      {
+        error ? <p className='font-weight-bold alert alert-danger text-center mt-4'>Something went wrong</p> : null
+      }
+      {
+        loading ? <p className='text-center'>Loading...</p> : null
+      }
 
       <table className='table table-striped'>
         <thead className='bg-primary table-dark'>
@@ -15,6 +40,16 @@ const Products = () => {
         </thead>
         
         <tbody>
+            {
+              allProducts === 0 ? `There are no products` : (
+                allProducts.map(product => (
+                  <Product 
+                    key={product.id}
+                    product={product}
+                  />
+                ))
+              )
+            }
 
         </tbody>
       </table>
