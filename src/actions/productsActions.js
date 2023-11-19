38ba05c592f660e607d701138156fa1153c17a93
Julian Barbosa <julianbarbossa@hotmail.com>
2023-11-18 19:28:25 -0500

    Add new product actions and functionality

--- src/actions/productsActions.js
+++ src/actions/productsActions.js
@@ -3,19 +3,49 @@
 import {
   ADD_PRODUCT,
   ADD_PRODCUT_SUCCESFULLY,
-  ADD_PRODUCT_ERROR
+  ADD_PRODUCT_ERROR,
+  START_DOWNLOAD_PRODUCTS,
+  DOWNLOAD_PRODUCTS_SUCCESS,
+  DOWNLOAD_PRODUCTS_ERROR,
+  GET_PRODUCT_DELETE,
+  PRODUCT_DELETE_SUCCES,
+  PRODUCT_DELETE_ERROR,
+  GET_PRODUCT_EDIT,
+  PRODUCT_EDIT_SUCCESS,
+  PRODUCT_EDIT_ERROR,
 } from '../types'
+
+import clientAxios from '../config/axios'
+
+import Swal from 'sweetalert2'
+
+
 
 //add new product
 
 export function addNewProductAction(product) {
-  return (dispatch) => {
+  return async (dispatch) => {
     dispatch(addProduct())
 
     try {
+      //Insert product in db
+      await clientAxios.post('/products',product)
       dispatch(saveProduct(product))
+      
+      //Alert
+      Swal.fire({
+        title: 'Correct',
+        text: 'Product add succesfully',
+        icon: 'success'
+      })
     } catch (error) {
+      console.log(error)
       dispatch(errorCreateProduct(true))
+      Swal.fire({
+        title: 'Error',
+        text: 'Something went wrong',
+        icon: 'error'
+      })
     }
   }
 }
@@ -33,4 +63,81 @@ const saveProduct = (product) => ({
 const errorCreateProduct = (stateError) => ({
   type: ADD_PRODUCT_ERROR,
   payload: stateError
+})
+
+// function to download product of the db
+export function getAllProducts(products) {
+  return async (dispatch) => {
+    dispatch(getProducts())
+
+    try {
+      const response = await clientAxios.get('/products')
+      // console.log(response)
+      dispatch(showProducts(response.data))
+    } catch (error) {
+      dispatch(errorShowProducts(true))
+      console.log(error)
+    }
+  }
+}
+
+const getProducts = () => ({
+  type: START_DOWNLOAD_PRODUCTS,
+  payload: true
+})
+
+const showProducts = (products) => ({
+  type: DOWNLOAD_PRODUCTS_SUCCESS,
+  payload: products
+})
+
+const errorShowProducts = (stateError) => ({
+  type: DOWNLOAD_PRODUCTS_ERROR,
+  payload: stateError
+})
+
+export function deleteProduct(id) {
+  return async (dispatch) => {
+    dispatch(productDeletedById(id))
+
+    try {
+      await clientAxios.delete(`products/${id}`)
+      // console.log(response)
+      dispatch(productDeletedSucces())
+      Swal.fire({
+        title: "Deleted!",
+        text: "Your product has been deleted.",
+        icon: "success"
+      });
+    } catch (error) {
+      dispatch(productDeletedError(true))
+      console.log(error)
+    }
+   
+  }
+}
+
+const productDeletedById = (id) => ({
+  type: GET_PRODUCT_DELETE,
+  payload: id
+})
+
+const productDeletedSucces = () => ({
+  type: PRODUCT_DELETE_SUCCES,
+})
+
+const productDeletedError = (stateError) => ({
+  type: PRODUCT_DELETE_ERROR,
+  payload: stateError
+})
+
+export function editProduct(product) {
+  return async (dispatch) => {
+    dispatch(getEditProduct(product))
+  }
+}
+
+const getEditProduct = (product) => ({
+  type: GET_PRODUCT_EDIT,
+  payload: product
 })
