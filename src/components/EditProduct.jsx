af167a9d50b15ba1eb7f120812196e68ea803ee8
Julian Barbosa <julianbarbossa@hotmail.com>
2023-11-20 12:16:45 -0500

    Refactor EditProduct component to include form submission and state management

--- src/components/EditProduct.jsx
+++ src/components/EditProduct.jsx
@@ -1,39 +1,83 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+
+import { useDispatch, useSelector } from "react-redux";
+import { editProductById } from "../actions/productsActions";
+
+import { useNavigate } from 'react-router-dom'
 
 const EditProduct = () => {
-  return (
-    <div className='row justify-content-center mt-5'>
-      <div className='col-md-8'>
-        <div className='card'>
-          <div className='card-body'>
-            <h2 className='text-center mb-4 font-weight-bold'>
-              Edit Product
-            </h2>
+  const [product, setSaveProduct] = useState({
+    name: "",
+    price: "",
+    id: "",
+  });
 
-            <form action="">
-              <div className='form-group'>
+  const navigation = useNavigate()
+
+  const dispatch = useDispatch();
+  const productEdit = useSelector((state) => state.products.productToEdit);
+
+  //Fill the state automatic
+  useEffect(() => {
+    if (productEdit) {
+      setSaveProduct(productEdit);
+    }
+  }, [productEdit]);
+
+  const { name, price, id } = product || {}; // Add a default value of an empty object
+
+  // Read data in the form
+  const onChangeForm = (e) => {
+    setSaveProduct({
+      ...product,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  
+  const submitProducEdited = (e) => {
+    e.preventDefault();
+    
+    dispatch(editProductById(product))
+    navigation('/')
+  };
+  
+
+  return (
+    <div className="row justify-content-center mt-5">
+      <div className="col-md-8">
+        <div className="card" key={id}>
+          <div className="card-body">
+            <h2 className="text-center mb-4 font-weight-bold">Edit Product</h2>
+
+            <form onSubmit={submitProducEdited}>
+              <div className="form-group">
                 <label htmlFor="">Name of the Product:</label>
-                <input 
-                  type="text" 
-                  className='form-control'
-                  placeholder='Name of the product'
-                  name='nameProduct'
-                />
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Name of the product"
+                  name="name"
+                  value={name}
+                  onChange={onChangeForm}
+                  />
               </div>
 
-              <div className='form-group'>
+              <div className="form-group">
                 <label htmlFor="">Price of the Product:</label>
-                <input 
-                  type="number" 
-                  className='form-control'
-                  placeholder='Price of the product'
-                  name='price'
-                />
+                <input
+                  type="number"
+                  className="form-control"
+                  placeholder="Price of the product"
+                  name="price"
+                  value={price}
+                  onChange={onChangeForm}
+                  />
               </div>
 
               <button
-                type='submit'
-                className='btn btn-primary font-weight-bold text-uppercase d-block w-100'
+                type="submit"
+                className="btn btn-primary font-weight-bold text-uppercase d-block w-100"
               >
                 Edit Product
               </button>
@@ -41,9 +85,8 @@ const EditProduct = () => {
           </div>
         </div>
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default EditProduct
+export default EditProduct;
