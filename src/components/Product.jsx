8a99a0e43058ee8f70ed44b0aee319a0774606c4
Julian Barbosa <julianbarbossa@hotmail.com>
2023-11-21 08:58:10 -0500

    Remove unused import statement

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import {  useNavigate } from 'react-router-dom'
 
 //redux
 import { useDispatch } from 'react-redux'
