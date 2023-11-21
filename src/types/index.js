a7a5c3d0d41d538a614805d0e6958adca5bc1ab6
Julian Barbosa <julianbarbossa@hotmail.com>
2023-11-21 09:02:40 -0500

    Add SHOW_ALERT and HIDE_ALERT constants

--- src/types/index.js
+++ src/types/index.js
@@ -17,5 +17,8 @@ export const PRODUCT_EDIT_SUCCESS = 'PRODUCT_EDIT_SUCCESS'
 export const PRODUCT_EDIT_ERROR = 'PRODUCT_EDIT_ERROR'
 
 
+export const SHOW_ALERT = 'SHOW_ALERT'
+export const HIDE_ALERT = 'HIDE_ALERT'
+
 
 
